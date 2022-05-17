3438749e6dc119a984d6dfe1bf5c556de5dd44af
BrunoBernardini <bruno.f.bernardini@gmail.com>
2022-05-17 19:40:57 +0200

    Componente header (+setup).

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <img src="../assets/img/logo.svg" alt="">
+    <select class="form-select top-select">
+      <option selected>Seleziona un genere</option>
+      <option value="1">Uno</option>
+      <option value="2">Due</option>
+      <option value="3">Tre</option>
+    </select>
+  </header>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/style/vars";
+@import "../assets/style/general";
+header{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 25px;
+  height: 69px;
+  background-color: $secondary-color;
+  img{
+    width: 38px;
+    filter: invert(48%) sepia(71%) saturate(448%) hue-rotate(84deg) brightness(95%) contrast(91%);
+  }
+  .top-select{
+    max-width: 193px;
+  }
+}
+</style>
